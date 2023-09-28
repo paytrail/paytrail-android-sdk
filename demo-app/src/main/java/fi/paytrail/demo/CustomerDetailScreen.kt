b57bbd57190d21c6ecc10923d3973f5f12aaee0e
Oussama baderkhane <baderkhaneoussama@gmail.com>
2023-09-28 14:18:54 +0300

    Adding Customer DetailScreen

---
+++ demo-app/src/main/java/fi/paytrail/demo/CustomerDetailScreen.kt
@@ -0,0 +1,208 @@
+package fi.paytrail.demo
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import fi.paytrail.demo.ui.theme.FilledButton
+import fi.paytrail.demo.ui.theme.MyColors.Grey03
+import fi.paytrail.demo.ui.theme.MyColors.Grey04
+import fi.paytrail.demo.ui.theme.OutLineButton
+
+@Composable
+fun CustomerDetailScreen(
+    modifier: Modifier = Modifier,
+    cancelAction: () -> Unit,
+    toPayAction: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxHeight()
+            .verticalScroll(rememberScrollState())
+            .padding(24.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        val focusManager = LocalFocusManager.current
+
+        val firstName = remember {
+            mutableStateOf("Maija")
+        }
+        val lastName = remember {
+            mutableStateOf("Neikalainen")
+        }
+        val phoneNumbere = remember {
+            mutableStateOf("044231122")
+        }
+        val checkBoxState = remember {
+            mutableStateOf(false)
+        }
+        val country = remember {
+            mutableStateOf("Finland")
+        }
+        Text(
+            text = "Customer Information",
+            style = MaterialTheme.typography.titleMedium,
+        )
+        Spacer(modifier = Modifier.height(26.dp))
+
+        FormInputField(placeholder = "First name",
+            imeAction = ImeAction.Next,
+            keyBoardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            ),
+            text = firstName.value,
+            onChange = {
+                firstName.value = it
+            }
+        )
+        Spacer(modifier = Modifier.height(26.dp))
+
+        FormInputField(placeholder = "Last name",
+            imeAction = ImeAction.Next,
+            keyBoardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            ),
+            text = lastName.value
+        )
+        Spacer(modifier = Modifier.height(26.dp))
+
+        FormInputField(placeholder = "Phone number",
+            imeAction = ImeAction.Next,
+            keyBoardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            ),
+            text = phoneNumbere.value
+        )
+
+        Spacer(modifier = Modifier.height(26.dp))
+
+        FormInputField(placeholder = "Email",
+            imeAction = ImeAction.Next,
+            keyBoardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            ),
+            text = phoneNumbere.value
+        )
+        Spacer(modifier = Modifier.height(26.dp))
+
+        FormInputField(placeholder = "Address",
+            imeAction = ImeAction.Next,
+            keyBoardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            ),
+            text = phoneNumbere.value
+        )
+        Spacer(modifier = Modifier.height(26.dp))
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            FormInputField(
+                modifier = modifier
+                    .weight(1f)
+                    .padding(end = 14.dp),
+                placeholder = "Zip code",
+                imeAction = ImeAction.Next,
+                keyBoardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                ),
+                text = phoneNumbere.value
+            )
+            FormInputField(placeholder = "Postal address",
+                modifier = modifier.weight(1f),
+                imeAction = ImeAction.Next,
+                keyBoardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                ),
+                text = phoneNumbere.value
+            )
+        }
+        Spacer(modifier = Modifier.height(26.dp))
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Checkbox(checked = checkBoxState.value, onCheckedChange = {
+                checkBoxState.value = it
+            })
+            Text(text = "I have read and accept the order and contract terms *")
+        }
+        Spacer(modifier = Modifier.height(26.dp))
+        FormInputField(placeholder = "Country",
+            imeAction = ImeAction.Next,
+            keyBoardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            ),
+            text = country.value
+        )
+        Spacer(modifier = Modifier.height(26.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+            OutLineButton(text = stringResource(id = R.string.payment_button_cancel)) {
+                cancelAction.invoke()
+            }
+            FilledButton(
+                text = stringResource(id = R.string.payment_button_payment),
+                isEnabled = checkBoxState.value
+            ) {
+                toPayAction.invoke()
+            }
+        }
+    }
+}
+
+@Composable
+fun FormInputField(
+    modifier: Modifier = Modifier,
+    text: String,
+    placeholder: String,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    onChange: (String) -> Unit = {},
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    keyBoardActions: KeyboardActions = KeyboardActions(),
+    isEnabled: Boolean = true
+) {
+    TextField(
+        modifier = modifier.fillMaxWidth(),
+        value = text,
+        onValueChange = onChange,
+        leadingIcon = leadingIcon,
+        textStyle = TextStyle(fontSize = 18.sp),
+        keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = keyboardType),
+        keyboardActions = keyBoardActions,
+        enabled = isEnabled,
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = Color.White,
+            focusedContainerColor = Color.White,
+            unfocusedIndicatorColor = Grey03,
+            unfocusedLabelColor = Grey04
+        ),
+        label = {
+            Text(text = placeholder)
+        }
+    )
+}
---
+++ demo-app/src/main/java/fi/paytrail/demo/ui/theme/Buttons.kt
@@ -0,0 +1,65 @@
+package fi.paytrail.demo.ui.theme
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import fi.paytrail.demo.ui.theme.MyColors.Grey02
+import fi.paytrail.demo.ui.theme.MyColors.PinkElement
+
+@Composable
+fun OutLineButton(text: String, onClick: () -> Unit) {
+    OutlinedButton(
+        modifier = Modifier
+            .widthIn(min = 80.dp)
+            .padding(top = 8.dp, end = 16.dp, start = 16.dp, bottom = 8.dp),
+        onClick = { onClick.invoke() },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = Grey02
+        ),
+        border = BorderStroke(1.dp, PinkElement)
+    ) {
+        Text(text)
+    }
+}
+
+@Composable
+fun FilledButton(text: String, isEnabled:Boolean = true, onClick: () -> Unit) {
+    Button(
+        modifier = Modifier
+            .widthIn(min = 80.dp)
+            .padding(top = 8.dp, end = 16.dp, start = 16.dp, bottom = 8.dp),
+        onClick = { onClick.invoke() },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = PinkElement,
+            contentColor = Color.White
+        ),
+        enabled = isEnabled
+    ) {
+        Text(text)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewButtons() {
+    Column {
+        OutLineButton("Cancel") {}
+        Spacer(modifier = Modifier.height(16.dp))
+        FilledButton("To Payment") {}
+        Spacer(modifier = Modifier.height(16.dp))
+        FilledButton("To Payment", isEnabled = false) {}
+    }
+}
